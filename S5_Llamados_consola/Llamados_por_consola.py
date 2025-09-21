6653522c53fa9218fe813c734f6d57f1dbfc1415
BelParia <brayandelacon@gmail.com>
2025-09-21 12:45:59 -0500

    feat(S5_Llamados_consola/): agrego teoría y ejercicios de llamado por consola

---
+++ S5_Llamados_consola/Llamados_por_consola.py
@@ -0,0 +1,12 @@
+# Entradas y salidas
+"""
+Claro 💡. En Python, la función input() permite hacer lo que llamas un “llamado por consola”:
+básicamente detener el programa y esperar que el usuario escriba algo desde el teclado, lo que
+convierte al script en interactivo. Por defecto, lo que devuelve siempre es una cadena de texto
+(string), aunque luego puedes transformarla a otros tipos de datos (como int() o float()). Por ejemplo,
+si escribes nombre = input("¿Cómo te llamas? "), el programa mostrará el mensaje y quedará en pausa
+hasta que el usuario escriba su respuesta, la cual se guardará en la variable nombre. Esto hace posible
+que los programas en Python reciban datos externos en tiempo de ejecución, en lugar de estar limitados
+solo a lo que ya está escrito en el código.
+"""
+
