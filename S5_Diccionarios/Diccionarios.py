f99284be588eefd0b1f2c9f6ce246930703f258e
BelParia <brayandelacon@gmail.com>
2025-09-21 09:54:45 -0500

    Actualizando el contenido de diccionarios

--- S5_Diccionarios/Diccionarios.py
+++ S5_Diccionarios/Diccionarios.py
@@ -9,7 +9,12 @@ estudiantes = {"nombre": "Gustavo", "edad": 27, "promedio": 17.5, "Casado": Fals
 ## Códigos
 """
 - variable["key"] = valor : te permite agregar una nueva key con su respectivo valor. Si existe el key
-entonces simplemente reemplazará el valor que se esté indicando
+entonces simplemente reemplazará el valor que se esté indicando.
 
-- 
+- del variable["key"] : en base a una key te permite eleminar la misma con su respectivo valor. Pero lo
+interesante es que si no existe ese key el programa te indicará un error (KeyError).
+
+- sorted: te permite obtener los nombres de los keys de un diccionario, y lo entrega con formato lista.
+
+-
 """
